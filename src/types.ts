11b5f7226bb7072a30db458646ed565a009d0b5d
bigmistqke <bigmistqke@users.noreply.github.com>
2023-10-24 23:50:48 +0000

    Format

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
-export type Vector = { x: number; y: number };
+export type Vector = { x: number; y: number }
 export type Handle = {
-  nodeId: string;
-  handleId: string;
-};
+  nodeId: string
+  handleId: string
+}
